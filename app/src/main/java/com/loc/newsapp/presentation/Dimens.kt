20415c3aeb6788e7f216cad9ee458603fcf833f9
rezky126 <rezky127@gmail.com>
2024-05-24 19:20:00 +0700

    created home screen viewmodel _ shimmer effect [Part 11]

--- app/src/main/java/com/loc/newsapp/presentation/Dimens.kt
+++ app/src/main/java/com/loc/newsapp/presentation/Dimens.kt
@@ -1,12 +1,18 @@
 package com.loc.newsapp.presentation
 
 import androidx.compose.ui.unit.dp
+import com.loc.newsapp.domain.model.Article
 
 object Dimens {
 
     val MediumPadding1 = 24.dp
     val MediumPadding2 = 30.dp
+    val ExtraSmallPadding = 3.dp
+    val ExtraSmallPadding2 = 6.dp
 
     val IndicatorSize = 20.dp
     val PageIndicatorWidth = 24.dp
+
+    val ArticleCardSize = 96.dp
+    val SmallIconSize = 11.dp
 }
--- app/src/main/java/com/loc/newsapp/util/Constants.kt
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -5,4 +5,5 @@ object Constants {
     const val USER_SETTINGS = "userSettings"
     const val APP_ENTRY = "appEntry"
     const val  API_KEY = "d50d6c3c710145b29b87c531493e244f"
+    const val BASE_URL = "https://newsapi.org/v2/"
 }
